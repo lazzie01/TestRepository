1194ae083036f022654438915b612c4693103b2a
lazzie <lazarus@warpdevelopment.com>
2020-09-09 14:13:36 +0200

    added hello

--- TestRepository/Program.cs
+++ TestRepository/Program.cs
@@ -10,6 +10,8 @@ namespace TestRepository
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Foxie");
+            Console.ReadLine();
         }
     }
 }
