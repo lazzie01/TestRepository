ca29799ffb6a025ad9137c90a8440e06e781d2c9
lazzie <lazarus@warpdevelopment.com>
2020-09-10 10:46:06 +0200

    Revert "added volume"
    This reverts commit 4f0230ea3ab648716aea70b51e75389a5db12363.

--- TestRepository/Program.cs
+++ TestRepository/Program.cs
@@ -10,19 +10,9 @@ namespace TestRepository
     {
         static void Main(string[] args)
         {
-            double h = 2.18;
-            double d = 1.74;
-            double pi = Math.PI;
-
-            double v = Volume(h,d,pi);
-
-            Console.WriteLine("Volume is: "+ v);
+            Console.WriteLine("Hello Foxie");
+            int x1 = 0;
             Console.ReadLine();
-        }
-
-        static double Volume(double h, double d, double pi)
-        {
-            return pi * (d / 2) * (d / 2) * h;
         }
     }
 }
