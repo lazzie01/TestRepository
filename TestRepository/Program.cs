4f0230ea3ab648716aea70b51e75389a5db12363
lazzie <lazarus@warpdevelopment.com>
2020-09-10 10:38:16 +0200

    added volume

--- TestRepository/Program.cs
+++ TestRepository/Program.cs
@@ -10,9 +10,19 @@ namespace TestRepository
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Foxie");
-            int x1 = 0;
+            double h = 2.18;
+            double d = 1.74;
+            double pi = Math.PI;
+
+            double v = Volume(h,d,pi);
+
+            Console.WriteLine("Volume is: "+ v);
             Console.ReadLine();
+        }
+
+        static double Volume(double h, double d, double pi)
+        {
+            return pi * (d / 2) * (d / 2) * h;
         }
     }
 }
